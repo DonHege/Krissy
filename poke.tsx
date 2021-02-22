365fcdaa069887a48d5936a61e71f17c45b0147c
Marco Hegenberg <marco_hegenberg@web.de>
2021-02-22 23:08:32 +0100

    Test1

---
+++ poke.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="poke" tilewidth="32" tileheight="32" tilecount="5250" columns="70">
+ <image source="tiles/poke.png" width="2240" height="2400"/>
+</tileset>
